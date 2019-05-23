ff731f6e011c8e642542a7124d7e72d2662f7305
Ganlv <ganlvtech@qq.com>
2019-05-23 16:28:33 +0800

    Add KnownEnphpBugsException test

--- tests/test.php
+++ tests/test.php
@@ -9,13 +9,34 @@ $filenames = scandir(SAMPLE_DIR);
 $filenames = array_values(array_diff($filenames, ['.', '..']));
 
 foreach ($filenames as $key => $filename) {
-    echo $filename, PHP_EOL;
-    $code = file_get_contents(SAMPLE_DIR . '/' . $filename);
+    $path = SAMPLE_DIR . '/' . $filename;
+    echo $path, PHP_EOL;
+    $code = file_get_contents($path);
     try {
         $code = \Ganlv\EnphpDecoder\AutoDecoder::decode($code);
         echo $code, PHP_EOL;
     } catch (Exception $e) {
         echo $e->getTraceAsString();
         exit(1);
+    }
+}
+
+define('BUG_SAMPLE_DIR', __DIR__ . '/bug_samples');
+$filenames = scandir(BUG_SAMPLE_DIR);
+$filenames = array_values(array_diff($filenames, ['.', '..']));
+
+foreach ($filenames as $key => $filename) {
+    $path = BUG_SAMPLE_DIR . '/' . $filename;
+    echo $path, PHP_EOL;
+    $code = file_get_contents($path);
+    try {
+        $code = \Ganlv\EnphpDecoder\AutoDecoder::decode($code);
+        echo $code, PHP_EOL;
+        exit(3);
+    } catch (\Ganlv\EnphpDecoder\KnownEnphpBugs\KnownEnphpBugsException $e) {
+        echo 'KnownEnphpBugsException: ', $e->getMessage(), PHP_EOL;
+    } catch (Exception $e) {
+        echo $e->getTraceAsString();
+        exit(2);
     }
 }
